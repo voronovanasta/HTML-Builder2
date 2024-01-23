b0f076c404a46cc04af3c5346a7fdf59e96adc2e
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-01-23 19:24:43 +0300

    refactor: change filepath

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,7 +9,8 @@ fs.readdir(path.resolve(__dirname, "secret-folder"),{
     files.forEach(file => {
         if(file.isFile()){
             let result = file.name.split('.')[0] + " - " + path.extname(file.name).slice(1) + " - ";
-            let filePath = file.path + "\\" + file.name;
+            //let filePath = file.path + "\\" + file.name;
+            let filePath = path.join(__dirname, 'secret-folder', file.name);
         fs.stat(filePath, (err, stats) => {
             if (err) {
               console.error(err);

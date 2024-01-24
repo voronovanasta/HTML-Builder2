bf8b5a3db45785493c945f11d2a5c4a87343586c
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-01-24 09:31:50 +0300

    fix: fix solution to delete file in files-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -11,6 +11,25 @@ function copyDir (){
         console.log('Directory created successfully!');
     });
 
+    fs.readdir(path.resolve(__dirname, "files-copy"),{
+        withFileTypes: true}, (err, files) => { 
+      if (err) 
+        console.log(err); 
+      else { 
+        files.forEach(file => {
+            let filePath = file.path + "\\" + file.name;
+            fs.unlink(path.resolve(__dirname, filePath), (err) => {
+                if (err) {
+                    console.log("Error Found:", err);
+                }
+                else {
+                    console.log("deleted file")
+                }
+            });
+        }) 
+      } 
+    }) 
+
 fs.readdir(path.resolve(__dirname, "files"),{
     withFileTypes: true}, (err, files) => { 
   if (err) 
@@ -30,4 +49,42 @@ fs.readdir(path.resolve(__dirname, "files"),{
   } 
 }) 
 }
+
+function copy (){ 
+    //console.log(path.join(__dirname, 'files-copy'))
+    function rm (){
+        console.log('if')
+        fs.rmdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) =>{ 
+            console.log(err); 
+        })
+    }
+   
+      
+    // fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }).then(function() { 
+    //     console.log('Directory created successfully'); 
+    // }).catch(function() { 
+    //     console.log('failed to create directory'); 
+    // }); 
+    
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) =>{ 
+            if(err){console.log(err)}
+            else{
+                console.log('created')
+            }; 
+      });
+
+fs.promises.readdir(path.resolve(__dirname, "files"),{
+    withFileTypes: true}).then((files) => {
+        files.forEach(file => {
+              let filePath = file.path + "\\" + file.name;
+              fs.promises.copyFile(path.resolve(__dirname, filePath), path.resolve(__dirname, "files-copy" + "\\" + file.name))
+              });
+          }) 
+        } 
+
+
 copyDir()
+
+
+
+
